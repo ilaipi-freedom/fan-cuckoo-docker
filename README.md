9f2b4ca06e1497a6dfe70acf7f7a61874681f74f
Billy Yang <mz.yyam@gmail.com>
2023-06-29 21:11:31 +0800

    auto create log / yara / .cwd

--- README.md
+++ README.md
@@ -5,4 +5,10 @@
 ```
 
 
+```shell
+
 cuckoo web runserver 0.0.0.0:8000
+
+# remove \r of start.sh
+sed -i 's/\r$//' start.sh
+```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if [ ! -d "/home/cuckoo/.cuckoo/log" ]; then
+    mkdir -p /home/cuckoo/.cuckoo/log
+fi
+
+if [ ! -d "/home/cuckoo/.cuckoo/yara" ]; then
+    mkdir -p /home/cuckoo/.cuckoo/yara
+fi
+
+if [ ! -f "/home/cuckoo/.cuckoo/.cwd" ]; then
+    touch /home/cuckoo/.cuckoo/.cwd
+fi
+
 # Check if __init__.py file does not exist
 if [ ! -f "/home/cuckoo/.cuckoo/__init__.py" ]; then
     cuckoo -d || true
